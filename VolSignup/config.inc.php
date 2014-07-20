6af8bc60e2af3a883c1456b587ba827c0d5a5514
twilson0813 <tom@itbytomwilson.com>
2014-07-20 10:45:36 -0400

    Update config.inc.php
    modify database url

--- VolSignup/config.inc.php
+++ VolSignup/config.inc.php
@@ -1,3 +1,3 @@
 <?php
- $db_host = "xdap373.bcbst.com";$db_user = "itbytomwilsondb";$db_pass = "Tntw2010";$db_name = "venue_itbytomwilson_com";$db_table = "volSignUp";
+ $db_host = "mysql.itbytomwilson.com";$db_user = "itbytomwilsondb";$db_pass = "Tntw2010";$db_name = "venue_itbytomwilson_com";$db_table = "volSignUp";
 ?>
